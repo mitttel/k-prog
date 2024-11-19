402449ba97a1f4013f63f6c4da27b7a2dfea7553
Igor <shcherbacovigor145@gmail.com>
2024-11-19 13:42:52 +0700

    add class matrix

---
+++ matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 461 on 19.11.2024.
+//
+
+#include "matrix.h"
---
+++ matrix.h
@@ -0,0 +1,16 @@
+//
+// Created by 461 on 19.11.2024.
+//
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+
+
+class matrix {
+
+};
+
+
+
+#endif //MATRIX_H
